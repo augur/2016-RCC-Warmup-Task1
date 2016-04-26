18a587c2d465862107bf68650988ddddffbde361
Denis Kilchichakov <denis@kilchichakov.com>
2016-04-26 10:10:23 +0300

    Mistake fix (removed > /dev/null in first test)

--- ruby_measure.sh
+++ ruby_measure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Ruby base solution (with diff):" &&
-time diff <( cat data/output.txt ) <( ruby solutions/solution1.rb < data/input.txt ) && > /dev/null
+time diff <( cat data/output.txt ) <( ruby solutions/solution1.rb < data/input.txt ) &&
 echo "Ruby base solution (> dev/null):" &&
 time ruby solutions/solution1.rb < data/input.txt > /dev/null
 
