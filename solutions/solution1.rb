0c4d0cb5de57a3b50510ffcaa2e6169a97dce44a
Denis Kilchichakov <denis@kilchichakov.com>
2016-04-28 20:41:31 +0300

    Fixed Ruby solution a bit, actualized tries_count again

--- solutions/solution1.rb
+++ solutions/solution1.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-code = STDIN.readline.rstrip!
-# tries_count unused in this implementation
-STDIN.readline
+code = STDIN.readline.chomp!
+tries_count = STDIN.readline.to_i
 
 code_bytes = code.bytes.to_a
 code_contains = Array.new(256, false)
@@ -11,10 +10,10 @@ code_bytes.each do |bt|
   code_contains[bt] = true
 end
 
-STDIN.each do |t|
+tries_count.times do
   total_right = 0
   missed_right = 0
-  t.rstrip!.each_byte.with_index do |bt, i|
+  STDIN.readline.chomp!.each_byte.with_index do |bt, i|
     if bt == code_bytes[i]
       total_right += 1
     else
